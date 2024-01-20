9df4c661a8bbff7c3e0af25c0d446ceb835b09ca
RichardIshimwe <ishimwerichard26@gmail.com>
2024-01-20 07:18:45 +0200

    fixed products api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\UserAuthenticationController;
 
 /*
 |--------------------------------------------------------------------------
